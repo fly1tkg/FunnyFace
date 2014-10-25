0ba3ee8df86fedc8828b3e571d2f8e6ecda38cd4
fly1tkg <fly1tkg@gmail.com>
2014-10-25 13:47:08 +0900

    first commit

---
+++ app/src/androidTest/java/nagoya/gdg/funnyface/ApplicationTest.java
@@ -0,0 +1,13 @@
+package nagoya.gdg.funnyface;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
